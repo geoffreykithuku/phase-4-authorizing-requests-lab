32a217cdb98d102aa081f7179ddd7dbab1f01a9f
Geoffrey Kithuku <mutembeikithuku12@gmail.com>
2022-11-29 18:47:51 +0300

    completed assignment

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,13 @@
 class ArticlesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
-
+ 
   def index
+    
     articles = Article.where(is_member_only: false).includes(:user).order(created_at: :desc)
-    render json: articles, each_serializer: ArticleListSerializer
+
+    
+      render json: articles, each_serializer: ArticleListSerializer
+   
   end
 
   def show
@@ -16,5 +20,7 @@ class ArticlesController < ApplicationController
   def record_not_found
     render json: { error: "Article not found" }, status: :not_found
   end
+  
+
 
 end
